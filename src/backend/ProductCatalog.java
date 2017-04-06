4dd1385a2ed40a9e3887723632283ac5f8c9fc64
Tamino Doerr <taminodoerr@gmx.de>
2017-04-06 09:17:28 +0200

    getList() gibt jetzt ein String-Array zurück

---
+++ src/backend/ProductCatalog.java
@@ -0,0 +1,14 @@
+package backend;
+
+public class ProductCatalog {
+    
+    private int i_id;
+    private String s_name;
+    
+    public ProductCatalog( String name ) {
+        
+        s_name= name;
+        
+    }
+
+}
