8395882fefcfb8fd0ad4958111268316e1988922
Tamino Doerr <psych0sis@gmx.net>
2017-04-13 18:00:37 +0200

    getList + getVariants geben jetzt einen Vector<String> zurück

--- src/backend/ProductCatalog.java
+++ src/backend/ProductCatalog.java
@@ -1,5 +1,7 @@
 package backend;
 
+import java.util.Vector;
+
 import com.sap.mw.jco.IRepository;
 import com.sap.mw.jco.JCO;
 import com.sap.mw.jco.JCO.Repository;
@@ -11,33 +13,34 @@ public class ProductCatalog
   public ProductCatalog(JCO.Client mConnection)
   {
       this.mConnection = mConnection;
+      connectionAttributes();
 
-//	  openConnectionToSAP(");//TODO
-	  //openConnectionToSAP(");
-    connectionAttributes();
-//    closeConnectionToSAP();
   }
 
-  public void connectionAttributes()
+  public void connectionAttributes()//<---das ist eine Entwickler-Test-Methode
   {
       //System.out.println(mConnection.getAttributes());
 
-	  JCO.Table tblCatalogs = getList();
-	  for (int i=0; i<tblCatalogs.getNumRows(); i++)
+	  Vector<String> v_ProductCatalogs = getList();
+	  for (int i=0; i<v_ProductCatalogs.size(); i++)
 	  {
-		  tblCatalogs.setRow(i);
-		  System.out.println(tblCatalogs.getString("PRODCAT"));
+		  System.out.println(v_ProductCatalogs.get(i));
 	  }
 
+      Vector<String> v_Variants = getVariants("PUMPS4SALE");
+      for (int i=0; i<v_Variants.size(); i++)
+      {
+          System.out.println(v_Variants.get(i));
+      }
 
 //In folgender Zeile wird er Name des Katalogsuebergeben
-	  JCO.Table tblVariants = getVariants("PUMPS4SALE");
+	 /* JCO.Table tblVariants = getVariants("PUMPS4SALE");
 	  for (int i=0; i<tblVariants.getNumRows(); i++)
 	  {
 		  tblVariants.setRow(i);
 		  System.out.println(tblVariants.getString("PRODCAT") + " --- " + tblVariants.getString("VARIANT"));
 	  }
-
+*/
 //In folgender Zeile wird der Name des Katalogs + der Variante uebergeben
 	  JCO.Table tblItems = getItems("PUMPS4SALE", "001");
 	  for (int i=0; i<tblItems.getNumRows(); i++)
@@ -57,23 +60,40 @@ public class ProductCatalog
   }
 
 
-  public JCO.Table getList()
+  public Vector<String> getList()
   {
 	  Repository mRepository = new JCO.Repository("MyRepository", mConnection);
 	  JCO.Function function = this.createFunction( mRepository, "BAPI_ADV_MED_GET_LIST");
 	  mConnection.execute( function);
+	  JCO.Table tbl_ProductCatalogs = function.getTableParameterList().getTable("CATALOGS");
 
-	  return function.getTableParameterList().getTable("CATALOGS");
+
+	  Vector<String> v_ProductCatalogs = new Vector<String>(tbl_ProductCatalogs.getNumRows(), 1);
+      for (int i=0; i<tbl_ProductCatalogs.getNumRows(); i++)
+         {
+              tbl_ProductCatalogs.setRow(i);
+              v_ProductCatalogs.add(tbl_ProductCatalogs.getString("PRODCAT"));
+         }
+      return v_ProductCatalogs;
   }
 
-  public JCO.Table getVariants(String s_catalog)
+  public Vector<String> getVariants(String s_catalog)
   {
 	  Repository mRepository = new JCO.Repository("MyRepository", mConnection);
 	  JCO.Function function = this.createFunction( mRepository, "BAPI_ADV_MED_GET_VARIANT_LIST");
 	  function.getImportParameterList().setValue(s_catalog, "CATALOG");
 	  mConnection.execute( function);
+	  JCO.Table tbl_Variants = function.getTableParameterList().getTable("VARIANTS");
 
-	  return function.getTableParameterList().getTable("VARIANTS");
+	  Vector<String> v_Variants = new Vector<String>(20, 1);
+	  for (int i=0; i<tbl_Variants.getNumRows(); i++)
+	  {
+	      tbl_Variants.setRow(i);
+	      v_Variants.add(tbl_Variants.getString("VARIANT"));
+	  }
+
+
+	  return v_Variants;
   }
 
   public JCO.Table getItems(String s_catalog, String s_variant)
